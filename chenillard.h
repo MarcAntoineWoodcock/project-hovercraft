a8d871fe1db559f9a4da109ee357519a5d0cb5da
Marc-Antoine Woodcock <33126725+MarcAntoineWoodcock@users.noreply.github.com>
2017-11-05 12:17:28 +0100

    Create chenillard.h

---
+++ chenillard.h
@@ -0,0 +1,14 @@
+/*
+ * chenillard.h
+ *
+ *  Created on: May 11, 2017
+ *      Author: Marc-Antoine
+ */
+
+#ifndef CHENILLARD_H_
+#define CHENILLARD_H_
+
+void DELAY(void);			//prototype de fonction DELAY
+void CHENILLARD(void);		//prototype de fonction DELAY
+
+#endif /* CHENILLARD_H_ */
