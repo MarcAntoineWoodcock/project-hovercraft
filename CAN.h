a85c7048d1ea415510cfca0966287318b342259b
Marc-Antoine Woodcock <33126725+MarcAntoineWoodcock@users.noreply.github.com>
2017-11-05 12:18:02 +0100

    Create CAN.h

---
+++ CAN.h
@@ -0,0 +1,15 @@
+/*
+ * can.h
+ *
+ *  Created on: May 11, 2017
+ *      Author: Marc-Antoine
+ */
+
+#ifndef CAN_H_
+#define CAN_H_
+void CAN_INIT(void);			// Prototype de la fonction CAN_INIT
+void CANBAT(void);				// Prototype de la fonction CANBAT
+unsigned char CAN_JOY1(void);	// Prototype de la fonction CAN_JOY1
+unsigned char CAN_JOY2(void);	// Prototype de la fonction CAN_JOY2
+
+#endif /* CAN_H_ */
